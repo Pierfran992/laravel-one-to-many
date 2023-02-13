c9c3dd0669758d8b80ee4192e4708ab3b2bab1cc
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-02-13 16:46:02 +0100

    Creata l'home page per stampare le info della persona e i suoi relativi post;

--- laravel-relation-ex-class/app/Http/Controllers/MainCotroller.php
+++ laravel-relation-ex-class/app/Http/Controllers/MainCotroller.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Person;
+use App\Models\PersonDetail;
+use App\Models\Post;
+
 class MainCotroller extends Controller
 {
     public function home() {
-        return view('pages.home');
+        $people = Person :: all();
+        return view('pages.home', compact('people'));
     }
 }
--- laravel-relation-ex-class/resources/views/pages/home.blade.php
+++ laravel-relation-ex-class/resources/views/pages/home.blade.php
@@ -1,5 +1,46 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <h1>hello world</h1> 
+
+    {{-- creo un ciclo foreach per stampare tutti le informazioni delle entità presenti nella tabella people --}}
+    @foreach ($people as $person)
+
+        <div class="card text-center my-3">
+
+            {{-- nome della persona che ha pubblicato il post --}}
+            <div class="card-header">
+                <h2>{{ $person -> first_name}} {{ $person -> last_name}}</h2>
+            </div>
+
+            {{-- container dei post --}}
+            {{-- stampo con un foreach tutti i post --}}
+                <div class="card-body d-flex flex-wrap gap-3">
+                    {{-- card contenente il post --}}
+                    @if ($person -> posts -> count() > 0)
+                        @foreach ($person -> posts as $post)
+                            <div class="card" style="width: 18rem;">
+                                <div class="card-body text-start">
+                                    <h5 class="card-title"><strong>Title:</strong> {{ $post -> title}}</h5>
+                                    <p class="card-text"><strong>Description:</strong> {{ $post -> body ? $post -> body : "No Description"}}</p>
+                                </div>
+                            </div>
+                        @endforeach
+                    @else
+                        <h2>You have not published any posts!&#128532;</h2>
+                    @endif
+                    
+                </div>
+
+            {{-- info sulla persona che ha pubblicato i post --}}
+            <div class="card-footer text-muted">
+                <h6><strong>Contacts:</strong></h6>
+                <span>Phone Number: {{ $person -> personDetail -> phone_number}}</span>
+                <br>
+                <span>Email: {{ $person -> personDetail -> email}}</span>
+                <br>
+                <span>Address: {{ $person -> personDetail -> address}}</span>
+            </div>
+        </div>
+
+    @endforeach
 @endsection
